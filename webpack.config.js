8b9dfff64d1b1ee95d1c7bd5db159c0fc864dbcd
Natallia Lastavenka <natasha.lastavenka@gmail.com>
2017-10-11 15:40:55 +0300

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
  
 const PATHS = {
     source: path.join(__dirname, 'source'),
     build: path.join(__dirname, 'build')
 };
  
-module.exports = {
+const common = merge([
+  {
     entry: PATHS.source + '/pages/index/index.js',
     output: {
         path: PATHS.build,
@@ -16,16 +20,19 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: PATHS.source + '/pages/index/index.pug'
         })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            }
-        ]
+    ]
+  },
+  pug()
+]);
+
+module.exports = function(env) {
+    if (env === 'production') {
+        return common;
+    }
+    if (env === 'development') {
+        return merge([
+            common,
+            devserver()
+        ]);
     }
 };
